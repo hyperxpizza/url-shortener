74a251e006bb75cf607475bdbf349f27f3714ca5
hyperxpizza <wojtek.frackowski@gmail.com>
2021-03-18 11:48:14 +0100

    progress update

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
-
 function App() {
+
+
+
   return (
     <div className="App">
       <header className="App-header">
-       	<h1>Hello World</h1>
+       	<h1>Short URL</h1>
+         
       </header>
     </div>
   );
